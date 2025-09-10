ce8ba6842bb977402a84f4fde80407d2a95286c4
Jimmy2004S <jimmy.jimenez@unicolombo.edu.co>
2025-09-09 19:25:18 -0500

    infra: update Terraform outputs for resources

---
+++ terraform/outputs.tf
@@ -0,0 +1,41 @@
+output "user_table_name" {
+  value = aws_dynamodb_table.user_table.id
+}
+
+output "avatars_bucket" {
+  value = aws_s3_bucket.avatars.id
+}
+
+output "password_secret_arn" {
+  value = aws_secretsmanager_secret.password_secret.arn
+}
+
+output "card_request_queue_url" {
+  value       = aws_sqs_queue.card_request_queue.url
+  description = "URL de la cola SQS para solicitudes de tarjeta"
+}
+
+output "register_lambda_arn" {
+  value       = aws_lambda_function.register.arn
+  description = "ARN de la Lambda de registro de usuario"
+}
+
+output "login_lambda_arn" {
+  value       = aws_lambda_function.login.arn
+  description = "ARN de la Lambda de login de usuario"
+}
+
+output "update_lambda_arn" {
+  value       = aws_lambda_function.updateProfile.arn
+  description = "ARN de la Lambda de actualización de usuario"
+}
+
+output "upload_lambda_arn" {
+  value       = aws_lambda_function.upload_avatar.arn
+  description = "ARN de la Lambda de subida de avatar"
+}
+
+output "get_lambda_arn" {
+  value       = aws_lambda_function.get_profile.arn
+  description = "ARN de la Lambda de obtención de perfil de usuario"
+}
